e26f75b89703e7857e708f9b8dd28d6e1beefe25
Kiran Aman <kwaman32@gmail.com>
2026-01-18 23:19:38 -0500

    stopped tracking build directory

--- build/spm/CMakeFiles/spm_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/spm_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/spm_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/spm/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/kiran-aman/Documents/spm_ws/install/spm_description/share/spm_description/package.sh"
